1c325012aa5374e44999f5e10c7df00b7562ba1b
Pahtreek <ugwuobinnaobinna@gmail.com>
2026-02-15 15:39:07 +0100

    fix port for render

--- server.js
+++ server.js
@@ -59,6 +59,6 @@ app.delete('/api/houses/:id', async (req, res) => {
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
     console.log(`Server is live on port ${PORT}!`);
 });
